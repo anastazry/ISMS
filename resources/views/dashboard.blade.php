834fe8fd5258e266c54266ba1afc3a447b4fd4a1
Your Name <you@example.com>
2024-06-22 18:28:17 +0800

    remove weird files

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,11 +23,7 @@
             }
         }
 
-        .card-container1 {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 20px;
-    }
+
 </style>
 @section('content')
 <div class="py-12" >
@@ -53,17 +49,7 @@
         </div>
         <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
         
-            <div class="card-container1" style="margin-top: 3%">
-                <a href="#" class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 uniform-card">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">HIRARC Verified</h5>
-                    {{-- <p class="font-normal text-gray-700 dark:text-gray-400">{{$hirarcReportCount}}</p> --}}
-                </a>
 
-                <a href="#" class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 uniform-card">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Incidents Investigated</h5>
-                    {{-- <p class="font-normal text-gray-700 dark:text-gray-400">{{$incidentInvestigationCount}}</p> --}}
-                </a>
-            </div>
         </div>
         <div class="grid grid-cols-2 gap-8" style="max-height: 80%">
             <a href="#" class="uniform-card block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
